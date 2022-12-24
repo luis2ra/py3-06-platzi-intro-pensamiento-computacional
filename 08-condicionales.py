2180f0fa0c02a091c8e3cb67a273aaa0d407bb05
Luis Altuve <luis2ra@gmail.com>
2022-12-23 22:28:31 -0500

    class practice 08

---
+++ 08-condicionales.py
@@ -0,0 +1,10 @@
+num1 = int(input("Escoge un primer numero entero: "))
+num2 = int(input("Escoge un segundo numero entero: "))
+
+
+if num1 > num2:
+    print("el primer numero es mayor que el segundo numero")
+elif num1 < num2:
+    print("el primer numero es menor que el segundo numero")
+else:
+    print("numeros iguales")
